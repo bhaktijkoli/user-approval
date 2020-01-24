416803a9fe78ab89af1490f69904033c1bd0c8e5
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-01-24 19:24:17 +0530

    Initial commit

---
+++ src/UserApprovalServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BhaktijKoli\UserApproval;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserApprovalServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+      
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
